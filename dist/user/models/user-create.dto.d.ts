b4bb92e1b14d9d68a6a131532f649b4ebcf14c58
AHProhor <ahprohor4@gmail.com>
2022-06-07 17:36:19 +0600

    roles and permissions added

--- dist/user/models/user-create.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class UserCreateDto {
-    first_name: string;
-    last_name: string;
-    email: string;
-}
--- dist/user/models/user-update.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class UserUpdateDto {
-    fist_name?: string;
-    last_name?: string;
-    email?: string;
-}
